a767eec744221ddbe913f721e71fac80f9540271
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-10-14 14:18:24 +0530

    axios connect with api

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
-import Header from '@/Components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body>
-        <Header />
+      <body className='p-12'>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import React from 'react';
-
-
+import React from 'react'
+import axios from 'axios'
 const page = () => {
   return (
     <div>
-      <h1>This is Home Page</h1>
+      <button className='px-5 py-3 bg-green-600 text-white font-bold'>Get Images</button>
+      <div className='p-10'>
+      </div>
     </div>
   )
 }
